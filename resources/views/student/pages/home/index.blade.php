90a4a19d17e25485ae8fe8664e94e485f2cef240
zaidanarif73 <zaidanarif37@gmail.com>
2025-02-02 20:44:40 +0700

    second touch test

--- resources/views/student/pages/home/index.blade.php
+++ resources/views/student/pages/home/index.blade.php
@@ -9,7 +9,7 @@
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-sm-10 col-lg-8">
-                                <h5 class="text-primary text-uppercase mb-3 animated slideInDown">KURSUS BAHASA PEMROGRAMAN ONLINE</h5>
+                                <h5 class="text-primary text-uppercase mb-3 animated slideInDown">KURSUS BAHASA PEMROGRAMAN ONLINE GES</h5>
                                 <h1 class="display-3 text-white animated slideInDown">Belajar Koding, Kuasai Masa Depan!</h1>
                                 <p class="fs-5 text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea sanctus eirmod elitr.</p>
                                 <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
